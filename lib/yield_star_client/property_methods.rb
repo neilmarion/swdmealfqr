edde39dda63d883d1306685afb7c1cb81ec58095
JP Moral <jp@aelogica.com>
2014-08-07 09:20:52 +0800

    [#75785372] Remove unmaintained comments and unnessary include

--- lib/yield_star_client/property_methods.rb
+++ lib/yield_star_client/property_methods.rb
@@ -1,31 +1,11 @@
 module YieldStarClient
   module PropertyMethods
-    include YieldStarClient::Validations
 
-    # Retrieves all properties for a client.
-    #
-    # @return [Array<YieldStarClient::Property>] list of properties
-    #
-    # @raise [ArgumentError] when client_name is missing or invalid
-    # @raise [YieldStarClient::AuthenticationError] when unable to authenticate to the web service
-    # @raise [YieldStarClient::OperationError] when the service raises an OperationError fault
-    # @raise [YieldStarClient::InternalError] when the service raises an InternalError fault
-    # @raise [YieldStarClient::ServerError] when any other server-side error occurs
     def get_properties
       response = GetProperties::Request.execute(default_savon_params)
       GetProperties::Response.new(response).properties
     end
 
-    # Retrieves information for a specific property.
-    #
-    # @param [String] external_property_id the ID of the property to obtain information for
-    # @return [YieldStarClient::Property] the property data
-    #
-    # @raise [ArgumentError] when external_property_id or client_name are missing or invalid
-    # @raise [YieldStarClient::AuthenticationError] when unable to authenticate to the web service
-    # @raise [YieldStarClient::OperationError] when the service raises an OperationError fault
-    # @raise [YieldStarClient::InternalError] when the service raises an InternalError fault
-    # @raise [YieldStarClient::ServerError] when any other server-side error occurs
     def get_property(external_property_id)
       response = GetProperty::Request.execute(
         default_savon_params.merge(external_property_id: external_property_id)
@@ -33,22 +13,12 @@ module YieldStarClient
       GetProperty::Response.new(response).property
     end
 
-
-    # Retrieves pricing parameters for a specific property.
-    #
-    # @param [String] external_property_id the ID of the property to obtain information for
-    # @return [YieldStarClient::PropertyParameters] the pricing data
-    #
-    # @raise [ArgumentError] when external_property_id or client_name are missing or invalid
-    # @raise [YieldStarClient::AuthenticationError] when unable to authenticate to the web service
-    # @raise [YieldStarClient::OperationError] when the service raises an OperationError fault
-    # @raise [YieldStarClient::InternalError] when the service raises an InternalError fault
-    # @raise [YieldStarClient::ServerError] when any other server-side error occurs
     def get_property_parameters(external_property_id)
       response = GetPropertyParameters::Request.execute(
         default_savon_params.merge(external_property_id: external_property_id)
       )
       GetPropertyParameters::Response.new(response).property_parameters
     end
+
   end
 end
