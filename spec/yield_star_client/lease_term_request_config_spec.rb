77af2de987e6d1ba5bd5e0767ae80a6d59414bc4
Ramon Tayag <ramon.tayag@gmail.com>
2014-10-21 11:00:57 +0800

    [#75232108] Include other params required by lease term rent to work

--- spec/yield_star_client/lease_term_request_config_spec.rb
+++ spec/yield_star_client/lease_term_request_config_spec.rb
@@ -12,6 +12,11 @@ module YieldStarClient
       subject { fake_request_class }
       it { is_expected.to have_attribute(:external_property_id, String) }
       it { is_expected.to have_attribute(:unit_number, String) }
+      it { is_expected.to have_attribute(:building, String) }
+      it { is_expected.to have_attribute(:min_lease_term, String) }
+      it { is_expected.to have_attribute(:max_lease_term, String) }
+      it { is_expected.to have_attribute(:first_move_in_date, Date) }
+      it { is_expected.to have_attribute(:last_move_in_date, Date) }
     end
 
     context "validations" do
@@ -21,13 +26,26 @@ module YieldStarClient
     end
 
     it "defines request_args" do
-      request = fake_request_class.new(unit_number: "unit_number")
+      request = fake_request_class.new(
+        unit_number: "unit_number",
+        building: "building",
+        min_lease_term: "min_lease_term",
+        max_lease_term: "max_lease_term",
+        first_move_in_date: "first_move_in_date",
+        last_move_in_date: "last_move_in_date",
+      )
 
       rent_options = double(
         to_request_hash: { rent_options: "rent_options" }
       )
-      allow(LeaseTermRentOptions).to receive(:new).
-        with(unit_number: "unit_number").and_return(rent_options)
+      allow(LeaseTermRentOptions).to receive(:new).with(
+        unit_number: "unit_number",
+        building: "building",
+        min_lease_term: "min_lease_term",
+        max_lease_term: "max_lease_term",
+        first_move_in_date: "first_move_in_date",
+        last_move_in_date: "last_move_in_date",
+      ).and_return(rent_options)
 
       expected_args = {
         unit_number: "unit_number",
