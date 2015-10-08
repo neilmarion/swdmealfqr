3342b18391806c48301aa0ea4920b0773557235e
Ramon Tayag <ramon.tayag@gmail.com>
2015-10-08 12:37:15 +0800

    Add available floorplan and unit factories [#104864702]

--- lib/yield_star_client/factories.rb
+++ lib/yield_star_client/factories.rb
@@ -23,4 +23,33 @@ FactoryGirl.define do
     make_ready_date "2015-01-02".to_date
   end
 
+  factory(:yield_star_client_available_floor_plan, {
+    class: YieldStarClient::AvailableFloorPlan,
+  }) do
+    external_property_id "321809"
+    floor_plan_name "1B1B-SM*"
+    effective_date "2015-02-08".to_date
+    square_feet 82
+    bedrooms 1
+    bathrooms 1
+    units []
+  end
+
+  factory(:yield_star_client_available_unit, {
+    class: YieldStarClient::AvailableUnit,
+  }) do
+    building "2999"
+    unit_type "2x1.5"
+    unit_number "201"
+    status :vacant
+    date_available "2015-08-01".to_date
+    base_market_rent 2000
+    base_concession 100
+    base_final_rent 1900
+    best_lease_term 12
+    best_market_rent 2500
+    best_concession 150
+    best_final_rent 2350
+  end
+
 end
