6cfcdfd39c32c8fafc163f0d1270db354035ca1f
Paula Torre <paula@aelogica.com>
2017-02-24 03:36:44 +0000

    [#138654543] Capture market and final rent
    Support ETL configuration

--- lib/yield_star_client/models/floor_plan.rb
+++ lib/yield_star_client/models/floor_plan.rb
@@ -12,13 +12,13 @@ module YieldStarClient
     attribute :units, Array
     attribute :id, String
     attribute :name, String
-    attribute :starting_rate, Integer
-    attribute :ending_rate, Integer
+    attribute :min_market_rent, Integer
+    attribute :max_market_rent, Integer
+    attribute :min_final_rent, Integer
+    attribute :max_final_rent, Integer
 
     def self.new_from_rent_summary_hash(hash)
       floorplan_id = hash.delete(:unit_type)
-      starting_rate = hash.delete(:min_market_rent)
-      ending_rate = hash.delete(:max_market_rent)
       square_feet = hash.delete(:avg_sq_ft)
       bedrooms = hash.delete(:bed_rooms)
       bathrooms = hash.delete(:bath_rooms)
@@ -27,8 +27,6 @@ module YieldStarClient
         hash.merge(
           id: floorplan_id,
           name: floorplan_id,
-          starting_rate: starting_rate,
-          ending_rate: ending_rate,
           square_feet: square_feet,
           bedrooms: bedrooms,
           bathrooms: bathrooms
--- spec/lib/yield_star_client/models/floor_plan_spec.rb
+++ spec/lib/yield_star_client/models/floor_plan_spec.rb
@@ -8,6 +8,8 @@ module YieldStarClient
           unit_type: "1BRa",
           min_market_rent: 10,
           max_market_rent: 30,
+          min_final_rent: 15,
+          max_final_rent: 35,
           avg_sq_ft: 800,
           bed_rooms: 2,
           bath_rooms: 1.5
@@ -19,8 +21,10 @@ module YieldStarClient
 
         expect(floor_plan.id).to eq "1BRa"
         expect(floor_plan.name).to eq "1BRa"
-        expect(floor_plan.starting_rate).to eq 10.0
-        expect(floor_plan.ending_rate).to eq 30.0
+        expect(floor_plan.min_market_rent).to eq 10.0
+        expect(floor_plan.max_market_rent).to eq 30.0
+        expect(floor_plan.min_final_rent).to eq 15.0
+        expect(floor_plan.max_final_rent).to eq 35.0
         expect(floor_plan.square_feet).to eq 800
         expect(floor_plan.bedrooms).to eq 2.0
         expect(floor_plan.bathrooms).to eq 1.5
